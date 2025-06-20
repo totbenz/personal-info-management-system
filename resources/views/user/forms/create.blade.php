00d19c9fcf4865ea841d6405fc1406571a5c5af3
Ikigai <19-1-01286@vsu.edu.ph>
2025-06-20 13:47:38 +0800

    Minor: Fix alignments, search bar and cancel button

--- resources/views/user/forms/create.blade.php
+++ resources/views/user/forms/create.blade.php
@@ -28,10 +28,10 @@
 
             <div class="mt-5 flex justify-end gap-x-4">
                 <div class="w-1/6">
-                    <x-button type="button" x-on:click="close" label="Cancel" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-danger hover:bg-danger-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-danger-700"/>
+                    <x-button type="button" x-on:click="close" label="Cancel" class="px-5 py-2.5 w-full bg-danger font-semibold text-xs text-white uppercase tracking-widest hover:hover:bg-red-600 hover:scale-105"/>
                 </div>
                 <div class="w-1/6">
-                    <x-button type="submit" label="Create" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-main hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"/>
+                    <x-button type="submit" label="Create" class="px-5 py-2.5 w-full bg-main font-semibold text-xs text-white uppercase tracking-widest hover:hover:bg-main_hover hover:scale-105 duration-150"/>
                 </div>
             </div>
         </form>
