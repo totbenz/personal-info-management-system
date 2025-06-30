5da78e1035d1408a457333d2c9e8a1340dd9b184
Ikigai <19-1-01286@vsu.edu.ph>
2025-06-30 16:04:06 +0800

    Patch: Modify download button

--- resources/views/livewire/datatable/loyalty-datatable.blade.php
+++ resources/views/livewire/datatable/loyalty-datatable.blade.php
@@ -35,7 +35,12 @@
                 </path>
             </svg>
             <a href="{{ route('loyalty-awards.export-pdf') }}" target="_blank">
-                <x-button class="ml-2 bg-green-600 hover:bg-green-700 text-white">Download PDF</x-button>
+                <x-button class="ml-2 bg-green-600 hover:bg-green-800 text-white flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                    </svg>
+                    PDF
+                </x-button>
             </a>
         </div>
     </div>
