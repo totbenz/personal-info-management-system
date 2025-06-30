9b4ceae9a395b96fd42bcaeb0dc92c0eeb7ebeea
Harley Gotardo <harleygothards2@gmail.com>
2025-06-30 22:36:47 +0800

    Replace notification dropdown with user info display
    Removed the notification dropdown from the navigation menu and replaced it with a display of the user's role and email. This change streamlines the UI by focusing on user information instead of notifications.

--- resources/views/livewire/navigation-menu.blade.php
+++ resources/views/livewire/navigation-menu.blade.php
@@ -64,35 +64,15 @@
 
             <div class="flex items-center space-x-2">
                 {{-- notification --}}
-                <div class="relative" x-data="{ open: false}">
-                    <button @click="open = !open" class="flex items-center p-1 hover:scale-110 hover:bg-[#18203b9e] hover:rounded-full duration-200">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-slate-100">
-                            <path fill-rule="evenodd" d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z" clip-rule="evenodd" />
-                        </svg>
-                    </button>
-
-                    <ul x-show="open"
-                        x-transition:enter="transition ease-out duration-200"
-                        x-transition:enter-start="opacity-0 scale-90"
-                        x-transition:enter-end="opacity-100 scale-100"
-                        x-transition:leave="transition ease-in duration-200"
-                        x-transition:leave-start="opacity-100 scale-100"
-                        x-transition:leave-end="opacity-0 scale-90"
-                        @click.away="open = false"
-                        class="absolute right-0 z-20 w-52 mt-2 py-1 bg-white rounded shadow border font-normal text-gray-500">
-                        <li>
-                            <a href="#" class="flex items-center justify-center py-2 hover:bg-gray-200">
-                                <span class="ml-2 text-xs">Tom liked one of your comments</span>
-                            </a>
-                        </li>
-                    </ul>
+                <div class="flex flex-col items-end mr-2">
+                    <span class="text-xs text-gray-300 font-semibold uppercase">{{ __(ucfirst(Auth::user()->role)) }}</span>
+                    <span class="text-xs text-gray-400">{{ Auth::user()->email }}</span>
                 </div>
                 {{-- profile  --}}
                 <div class="relative" x-data="{ open: false }">
                     <button @click="open = !open" class="flex items-center rounded-full">
                         <x-user-icon></x-user-icon>
                     </button>
-
 
                     <ul x-show="open"
                         x-transition:enter="transition ease-out duration-200"
@@ -104,11 +84,9 @@
                         @click.away="open = false"
                         class="absolute z-20 right-0 w-48 mt-2 py-1 bg-white rounded shadow border font-normal text-gray-500">
 
+
                         <li class="border-gray-400">
                             <a href="{{ route('settings') }}" class="flex items-center px-3 py-1.5 hover:bg-gray-200">
-                                <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                                </svg> -->
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.01c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.01 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0-1.01 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0-2.572 1.01c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0-2.573-1.01c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0-1.01-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.01-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.573-1.01z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
